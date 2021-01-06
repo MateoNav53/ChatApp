b7af5ccd3cff99c2b8b651c148e35bbbc58e3acc
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2021-01-06 15:25:34 -0800

    added working api and user model

--- App.tsx
+++ App.tsx
@@ -1,20 +1,72 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useEffect } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
 
-import Amplify from "aws-amplify";
+import Amplify, { Auth, API, graphqlOperation } from "aws-amplify";
 import config from "./aws-exports";
+import { getUser } from "./graphql/queries";
+import { createUser } from "./graphql/mutations";
 import { withAuthenticator } from "aws-amplify-react-native";
 
 Amplify.configure(config);
 
+const randomImages = [
+  "https://hieumobile.com/wp-content/uploads/avatar-among-us-2.jpg",
+  "https://hieumobile.com/wp-content/uploads/avatar-among-us-3.jpg",
+  "https://hieumobile.com/wp-content/uploads/avatar-among-us-6.jpg",
+  "https://hieumobile.com/wp-content/uploads/avatar-among-us-9.jpg",
+];
+
 function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  const getRandomImage = () => {
+    return randomImages[Math.floor(Math.random() * randomImages.length)];
+  };
+
+  //run this snippet when app first mounts(loads)
+  useEffect(() => {
+    const fetchUser = async () => {
+      //get authenticated user from Auth
+      //we use bypassCache to make sure we are taking it from the server, not the cache
+      const userInfo = await Auth.currentAuthenticatedUser({
+        bypassCache: true,
+      });
+
+      //get the user from backend with user Id(sub) from Auth
+      if (userInfo) {
+        const userData = await API.graphql(
+          graphqlOperation(getUser, { id: userInfo.attributes.sub })
+        );
+        console.log(userData);
+
+        if (userData.data.getUser) {
+          console.log("User is already registered in database");
+          return;
+        }
+
+        const newUser = {
+          id: userInfo.attributes.sub,
+          name: userInfo.username,
+          imageUri: getRandomImage(),
+          status: "Example Status",
+        };
+
+        console.log(newUser);
+
+        await API.graphql(graphqlOperation(createUser, { input: newUser }));
+      }
+
+      //If there is no user in our database w/ the id, then create one
+    };
+
+    fetchUser();
+  }, []);
 
   if (!isLoadingComplete) {
     return null;
